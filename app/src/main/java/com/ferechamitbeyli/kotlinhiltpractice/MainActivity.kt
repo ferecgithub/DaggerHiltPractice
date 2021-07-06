9e762276c19eea0640e94e8240d01c338e2387cd
ferecgithub <ferechamitbeyli@gmail.com>
2021-07-07 00:08:38 +0200

    Scoping with Hilt

--- app/src/main/java/com/ferechamitbeyli/kotlinhiltpractice/MainActivity.kt
+++ app/src/main/java/com/ferechamitbeyli/kotlinhiltpractice/MainActivity.kt
@@ -2,8 +2,11 @@ package com.ferechamitbeyli.kotlinhiltpractice
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.Fragment
 import dagger.hilt.android.AndroidEntryPoint
+import dagger.hilt.android.scopes.ActivityScoped
 import javax.inject.Inject
+import javax.inject.Singleton
 
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
@@ -22,10 +25,28 @@ class MainActivity : AppCompatActivity() {
 }
 
 /**
+ * A class with SingletonScope is injected
+ * to MyFragment class via Field Injection.
+ * It will work unless we use FragmentScope
+ * on SomeClass.
+ * !! Tier system works downwards. For example,
+ * A FragmentScore can't be used in a SingletonScope.
+ */
+@AndroidEntryPoint
+class MyFragment : Fragment() {
+
+    // Field injection in Fragment class
+    @Inject
+    lateinit var someClass: SomeClass
+}
+
+/**
  * EXAMPLE CLASS FOR FIELD INJECTION
  * Hilt basically creates and holds SomeClass in memory
  * and when it's necessary, injects it.
  */
+//@Singleton
+@ActivityScoped
 class SomeClass
 @Inject
 constructor(
@@ -50,5 +71,14 @@ constructor() {
         return "Look I did some other thing!"
     }
 }
+
+/**
+ * Tier system for Scope components, from long living to the bottom:
+ * 1- SingletonScope : Lives as long as application lives
+ * 2- ActivityRetainedScope : Basically scope of the ViewModel. Lives longer than Activity but less than the application itself.
+ * 3- ActivityScope : Only lives as long as activity
+ * 4- FragmentScope : Only lives as long as fragment
+ * Docs: https://developer.android.com/training/dependency-injection/hilt-android#component-scopes
+ */
 
 
