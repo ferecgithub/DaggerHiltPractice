d016b88936c3efc2352245fd8c5721ac0a7285ce
ferecgithub <ferechamitbeyli@gmail.com>
2021-07-07 21:29:10 +0200

    @Binds implementation

--- app/src/main/java/com/ferechamitbeyli/kotlinhiltpractice/MainActivity.kt
+++ app/src/main/java/com/ferechamitbeyli/kotlinhiltpractice/MainActivity.kt
@@ -2,10 +2,13 @@ package com.ferechamitbeyli.kotlinhiltpractice
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import com.google.gson.Gson
+import dagger.Binds
+import dagger.Module
+import dagger.hilt.InstallIn
 import dagger.hilt.android.AndroidEntryPoint
-import dagger.hilt.android.scopes.ActivityScoped
+import dagger.hilt.components.SingletonComponent
+import dagger.hilt.android.qualifiers.ApplicationContext
 import javax.inject.Inject
 import javax.inject.Singleton
 
@@ -28,22 +31,24 @@ class MainActivity : AppCompatActivity() {
  * WHEN YOU TRY TO INJECT AN INTERFACE!!
  * YOU ALSO CAN'T INJECT 3RD PARTY LIBRARY
  * (THE CLASSES WE DON'T HAVE REAL IMPLEMENTATION)
+ * HILT OFFERS @Binds AND @Provides AS WORKAROUNDS.
+ *  - @Binds is used for interface implementations.
+ *  - @Provides is used for other 3rd party libraries. (Also works for all scenarios however underlying generated code is worse.)
  */
 class SomeClass
 @Inject
 constructor(
-    //private val someInterfaceImpl: SomeInterface,
-    private val gson: Gson
+    private val someInterfaceImpl: SomeInterface
+   // private val gson: Gson
 ){
     fun doAThing(): String{
-        //return "Look I got: ${someInterfaceImpl.getAThing()}"
-        return "Gson injected, or is it?"
+        return "Look I got: ${someInterfaceImpl.getAThing()}"
     }
 }
 
 class SomeInterfaceImpl
 @Inject
-constructor() : SomeInterface{
+constructor() : SomeInterface {
     override fun getAThing(): String {
         return "A Thing"
     }
@@ -52,4 +57,25 @@ constructor() : SomeInterface{
 
 interface SomeInterface{
     fun getAThing(): String
+}
+
+@InstallIn(SingletonComponent::class) // These dependencies will live as long as the application lives.
+@Module
+abstract class MyModule { // Abstract class for @Binds implementation
+
+    @Singleton
+    @Binds
+    abstract fun bindSomeDependency(
+        someImpl: SomeInterfaceImpl
+    ): SomeInterface
+
+    // @Binds doesn't work for Gson
+    /*
+    @Singleton
+    @Binds
+    abstract fun bindGson(
+        gson: Gson
+    ): Gson
+
+     */
 }
